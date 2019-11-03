d26791a254fa8f044241a544ca06432c2789483a
Andre Alexandre <andre.alexandre@zalando.ie>
2019-11-03 18:09:24 +0000

    #1 add jquery dependency and improved detection for jira web pages

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,4 +1,5 @@
 import ext from "./utils/ext";
+import $ from "jquery";
 
 var extractTags = () => {
   var url = document.location.href;
@@ -33,10 +34,29 @@ function onRequest(request, sender, sendResponse) {
 }
 
 function onStartup() { 
-  console.log('onStartup')
-  let jiraIssueHeader = document.getElementById('jira-issue-header')
-  if(jiraIssueHeader) {
-    jiraIssueHeader.style.backgroundColor = 'red'
+  detectJiraCsvExport()
+}
+
+function detectJiraCsvExport() {
+  let jiraIssueHeader = $('.aui-page-header-main')
+  if(jiraIssueHeader.length) {
+    console.log('JIRA issue header exists')
+    // jiraIssueHeader.css('background-color', 'red')
+    findCsvExport()
+  }
+  else {
+    console.warn('JIRA issue header DOESN\'T exists')
+  }
+}
+
+function findCsvExport() {
+  let csvAnchor = $('#jira\\.issueviews\\:issue-csv')
+  if(csvAnchor.length) {
+    console.log("Found anchor for JIRA CSV export")
+    console.log(csvAnchor)
+  }
+  else {
+    console.log("No CSV export found for this JIRA")
   }
 }
 
