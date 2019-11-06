8f875a6978460a593f3f1d8c240ea8746dcdd941
André Alexandre <andrealexandre316@gmail.com>
2019-11-06 11:37:47 +0000

    Merge Xamarin app to master and tag version (#2)
    * Added code base for log processor
    
    * added travis build config and updated README
    
    * updated readme
    
    * travis build config file updated
    
    * first setup for github actions CI
    
    * trying example from azure pipelines
    
    * setup xamarin sdk
    
    * fixed unsupported property
    
    * setup update
    
    * xamarin build setup
    
    * try to install tools
    
    * trying to install mkbundle
    
    * try to setup xamarin tools
    
    * update
    
    * bump
    
    * bump

---
+++ LogWorkProcessor/ViewController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using AppKit;
+using Foundation;
+
+namespace LogWorkProcessor
+{
+    public partial class ViewController : NSViewController
+    {
+        private int count = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void Button_Action(NSObject sender)
+        {
+            ++count;
+            Console.WriteLine($"Button Pressed {count} times");
+            Label.StringValue = "";
+
+            var openPanel = new NSOpenPanel();
+            openPanel.CanChooseFiles = true;
+            openPanel.CanChooseDirectories = false;
+            openPanel.CanCreateDirectories = false;
+            openPanel.RunModal(new string[] { "csv" });
+
+            //busy work to strip url
+            var url = openPanel.Url;
+            var fileUrlEncoded = url.FilePathUrl.AbsoluteString.Replace("file://", "");
+            var fileUrl = Uri.UnescapeDataString(fileUrlEncoded);
+
+            //UI updates
+            Console.WriteLine($"Selected file {fileUrl}");
+            Label.StringValue += $"Selected file {fileUrl}";
+
+            //Call csv processor (this should be delegated to worker thread)
+            Task.Factory
+                .StartNew(() => new CsvProcessor().Process(fileUrl))
+                .ContinueWith((resultFile) => 
+                {
+                    Console.WriteLine($"Total log work sheet by user created in file {resultFile.Result}");
+
+                    //Update UI
+                    InvokeOnMainThread(() =>
+                    {
+                        Label.StringValue += $"\n\nTotal log work sheet by user created in file {resultFile.Result}";
+                    });
+                });
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # jira-log-processor
-A simple MacApp for processing time logs in Jira's csv exports
+
+A simple Mac application for aggregating time logs in Jira's csv exports.
+
+## Build status
+
+[![Build status](https://travis-ci.com/andrealexandre/jira-log-processor.svg?token=iCkpZ2HV6bDiMrH9yTsw&branch=develop)](https://github.com/andrealexandre/jira-log-processor)
+TODO: code coverage
+[![Download latest](https://github.com/andrealexandre/jira-log-processor/releases/download/v1.0/LogWork.Processor.zip)]
+
+## Tech stack
+
+* C# 4.4.0
+* Xamarin
+* CsvHelper 7.1.1
